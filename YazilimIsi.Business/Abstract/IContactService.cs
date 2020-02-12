da2e4ca2680dda015d7f08bb70f403b278b3c20c
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2020-02-12 23:37:07 +0300

    Add Business Layer Base

---
+++ YazilimIsi.Business/Abstract/IContactService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YazilimIsi.Entity.Models;
+
+namespace YazilimIsi.Business.Abstract
+{
+    public interface IContactService
+    {
+        List<Contact> GetAllContactMessages();
+        Contact GetMessageById(int Id);
+        void Add(Contact contact);
+        void Update(Contact contact);
+        void Delete(Contact contact);
+    }
+}
---
+++ YazilimIsi.Business/Concrete/ContactManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YazilimIsi.Business.Abstract;
+using YazilimIsi.DataAccess.Abstract;
+using YazilimIsi.Entity.Models;
+
+namespace YazilimIsi.Business.Concrete
+{
+    public class ContactManager : IContactService
+    {
+
+        private IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+
+        /* Yeni Bir Mesaj Ekleme */
+        public void Add(Contact contact)
+        {
+            _contactDal.Add(contact);
+        }
+
+        /* Bir Mesaj Silme */
+        public void Delete(Contact contact)
+        {
+            _contactDal.Delete(contact);
+        }
+
+        /* Butun Mesajlari Getirme */
+        public List<Contact> GetAllContactMessages()
+        {
+            return _contactDal.GetAll();
+        }
+
+        /* Bir Mesaj Getirme */
+        public Contact GetMessageById(int Id)
+        {
+            return _contactDal.Get(x => x.Id == Id);
+        }
+
+        /* Bir Mesaj Guncelleme */
+        public void Update(Contact contact)
+        {
+            _contactDal.Update(contact);
+        }
+    }
+}
