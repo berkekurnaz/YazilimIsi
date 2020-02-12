4f48067e3ec529342e1e550be44ba79d89636223
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2020-02-12 23:57:24 +0300

    Add Web App Configuration

---
+++ YazilimIsi.WebApp/Controllers/AnasayfaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using YazilimIsi.Business.Abstract;
+using YazilimIsi.Business.Concrete;
+using YazilimIsi.DataAccess.Concrete;
+
+namespace YazilimIsi.WebApp.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+
+        IContactService _contactService = new ContactManager(new EfContactDal());
+
+        public IActionResult Index()
+        {
+            return View(_contactService.GetAllContactMessages());
+        }
+
+    }
+}
--- YazilimIsi.WebApp/Startup.cs
+++ YazilimIsi.WebApp/Startup.cs
@@ -5,19 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using YazilimIsi.Entity.Models;
 
 namespace YazilimIsi.WebApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DbYazilimIsiContext>();
+
+            services.AddMvc();
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -25,10 +37,14 @@ namespace YazilimIsi.WebApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Anasayfa}/{action=Index}/{id?}");
             });
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
         }
     }
 }
