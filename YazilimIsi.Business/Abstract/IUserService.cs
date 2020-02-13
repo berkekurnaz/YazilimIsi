117fa0eda96ef0cf19c694aea0c1e4c646cf002c
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2020-02-13 19:34:57 +0300

    Add User Base Operations

---
+++ YazilimIsi.Business/Abstract/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YazilimIsi.Entity.Models;
+
+namespace YazilimIsi.Business.Abstract
+{
+    public interface IUserService
+    {
+        List<User> GetAllUsers();
+        User GetUserById(int Id);
+        void Add(User user);
+        void Update(User user);
+        void Delete(User user);
+    }
+}
---
+++ YazilimIsi.Business/Concrete/UserManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YazilimIsi.Business.Abstract;
+using YazilimIsi.DataAccess.Abstract;
+using YazilimIsi.Entity.Models;
+
+namespace YazilimIsi.Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+
+        private IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        /* Yeni Bir Kullanici Ekleme */
+        public void Add(User user)
+        {
+            _userDal.Add(user);
+        }
+
+        /* Bir Kullanici Silme */
+        public void Delete(User user)
+        {
+            _userDal.Delete(user);
+        }
+
+        /* Butun Kullanicilari Getirme */
+        public List<User> GetAllUsers()
+        {
+            return _userDal.GetAll();
+        }
+
+        /* Bir Kullanici Getirme */
+        public User GetUserById(int Id)
+        {
+            return _userDal.Get(x => x.Id == Id);
+        }
+
+        /* Bir Kullanici Guncelleme */
+        public void Update(User user)
+        {
+            _userDal.Update(user);
+        }
+    }
+}
